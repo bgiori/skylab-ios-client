9391bd8cb3a167803536b5e93c8fae3f02966587
Brian Giori <brian.giori@amplitude.com>
2021-05-05 16:43:35 -0700

    finalize release config

--- release.config.js
+++ release.config.js
@@ -54,9 +54,7 @@ module.exports = {
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
     ["@semantic-release/exec", {
-      //"publishCmd": "pod trunk push AmplitudeSkylab.podspec",
-      //"successCmd": "swift doc generate Sources/Skylab/ --module-name Skylab --output docs --format html --base-url /skylab-ios-client && git commit -am '${nextRelease.version}' && git push"
-      "publishCmd": "echo YEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEET",
+      "publishCmd": "pod trunk push AmplitudeSkylab.podspec",
     }],
   ],
 }
